b395be51c7a938ef88cff39f1a81173ec17b2301
Rostam Shirani <rostams@lyft.com>
2021-05-30 19:01:35 -0700

    Adding Deployment/Migration file for SupplyChainApp

---
+++ SupplyChainApp/migrations/1_initial_erc20Token.js
@@ -0,0 +1,7 @@
+const ERC20Token = artifacts.require("./erc20Token.sol");
+const SupplyChain = artifacts.require("./SupplyChain.sol");
+
+module.exports = function (deployer) {
+  deployer.deploy(ERC20Token, 10000, "TotalSem Token", 18, "TotalSem");
+  deployer.deploy(SupplyChain)
+};
